12e4a02d7a2ea2adbdf6e891f2ff12dd8364fc62
Arvind Naidu <k.arvindnaidu@gmail.com>
2023-09-24 13:26:02 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Box,
+  Container,
+  Typography,
+  Button,
+  AppBar,
+  Toolbar,
+  IconButton
+} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="xl">
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography variant="h6" color="inherit" component="div">
+              Admin Dashboard
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Container>
     </div>
   );
 }
